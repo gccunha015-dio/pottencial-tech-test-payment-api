9a9e80b4a9ea46867f43aa03b80d8d6d41c8b472
Gabriel C. Cunha <gccunha015@gmail.com>
2022-09-09 14:33:45 -0300

    Modelo Item atualizado

--- PaymentAPI/PaymentAPI/Models/Item.cs
+++ PaymentAPI/PaymentAPI/Models/Item.cs
@@ -4,6 +4,8 @@ public class Item
 {
   private uint _id;
   private string _nome;
+  public uint Id { get => _id; }
+  public string Nome { get => _nome; }
   public decimal PrecoUnitario { get; set; }
   public Item(uint id, string nome, decimal precoUnitario)
   {
